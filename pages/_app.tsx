1401cbc011f3efeb151cef43c2e5fbd1661d773b
neolink78 <thibaut.resse@outlook.fr>
2025-04-22 15:44:41 +0200

    added background and reset base text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import {appWithTranslation} from 'next-i18next'
 import '@/styles/globals.css'
 import { AppProps } from 'next/app';
-
+import ParticlesBackground from '@/components/Particles';
 
 function App({Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return <>
+    <ParticlesBackground />
+    <Component {...pageProps} />
+    </>
 }
 
 export default appWithTranslation(App)
